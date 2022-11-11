88bdf73c1a0b3791542e031d116f3dae2400c9df
YuliaChen8179 <spirit6135@gmail.com>
2022-11-11 22:10:30 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,57 @@
+// 載入 express 並建構應用程式伺服器
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+
+const mongoose = require('mongoose') // 載入 mongoose
+const Todo = require('./models/todo')
+const bodyParser = require('body-parser') // 引用 body-parser
+
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render("new")
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
